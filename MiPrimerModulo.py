ecd267f0ca0363ab0bbb03c65d501865ab7e877a
Andres Jimenez <148875636+andr3sjimenez@users.noreply.github.com>
2023-10-25 17:03:09 -0500

    Update MiPrimerModulo.py

--- MiPrimerModulo.py
+++ MiPrimerModulo.py
@@ -1,39 +1,28 @@
-# MiPrimerModulo.py
-
-import statistics
 import math
 
-def Suma5(*args):
-    if len(args) != 5:
-        raise ValueError("Proporcione 5 números")
-    return sum(args)
+# Función para sumar 5 registros
+def sum_5(a, b, c, d, e):
+    return a + b + c + d + e
 
-def Potencia5(*args):
-    if len(args) != 5:
-        raise ValueError("Proporcione 5 números")
-    return [x ** 2 for x in args]
+# Función que recibe 5 números y los eleva al cuadrado
+def power_5(a, b, c, d, e):
+    return (a ** 2, b ** 2, c ** 2, d ** 2, e ** 2)
 
-def Raiz5(*args):
-    if len(args) != 5:
-        raise ValueError("Proporcione 5 números")
-    return [math.sqrt(x) for x in args]
+# Función que recibe 5 números y obtiene la raíz cuadrada de cada uno
+def square_5(a, b, c, d, e):
+    return (math.sqrt(a), math.sqrt(b), math.sqrt(c), math.sqrt(d), math.sqrt(e))
 
-def Varianza5(*args):
-    if len(args) != 5:
-        raise ValueError("Proporcione 5 números")
-    return statistics.variance(args)
+# Función que recibe 5 números y se obtiene la varianza muestral
+def var_5(a, b, c, d, e):
+    mean = (a + b + c + d + e) / 5
+    variance = ((a - mean) ** 2 + (b - mean) ** 2 + (c - mean) ** 2 + (d - mean) ** 2 + (e - mean) ** 2) / 4
+    return variance
 
-def asimetria_curtosis5(*args):
-    if len(args) != 5:
-        raise ValueError("Proporcione 5 números")
-    
-    mean = statistics.mean(args)
-    variance = statistics.variance(args)
-    
-    # Cálculo de la asimetría
-    skewness = sum((x - mean) ** 3 for x in args) / (len(args) * variance ** (3/2))
-    
-    # Cálculo de la curtosis
-    kurtosis = sum((x - mean) ** 4 for x in args) / (len(args) * variance ** 2) - 3
-    
+# Función que recibe 5 números y se obtiene el coeficiente de asimetría y curtosis
+def curto_5(a, b, c, d, e):
+    mean = (a + b + c + d + e) / 5
+    variance = ((a - mean) ** 2 + (b - mean) ** 2 + (c - mean) ** 2 + (d - mean) ** 2 + (e - mean) ** 2) / 4
+    skewness = ((a - mean) ** 3 + (b - mean) ** 3 + (c - mean) ** 3 + (d - mean) ** 3 + (e - mean) ** 3) / (5 * variance ** 1.5)
+    kurtosis = ((a - mean) ** 4 + (b - mean) ** 4 + (c - mean) ** 4 + (d - mean) ** 4 + (e - mean) ** 4) / (5 * variance ** 2)
     return skewness, kurtosis
+
